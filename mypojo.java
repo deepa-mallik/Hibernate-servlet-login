d9beee31b4765905007c58f242ecae467f5169aa
deepa-mallik <57281904+deepa-mallik@users.noreply.github.com>
2019-11-02 12:36:54 +0530

    Add files via upload

---
+++ mypojo.java
@@ -0,0 +1,23 @@
+public class mypojo
+{
+	String uname,pword,cpw;
+	public String getCpw() {
+		return cpw;
+	}
+	public void setCpw(String cpw) {
+		this.cpw = cpw;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPword() {
+		return pword;
+	}
+	public void setPword(String pword) {
+		this.pword = pword;
+	}
+	
+}
